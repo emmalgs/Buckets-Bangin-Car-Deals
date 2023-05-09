3d54d5d7679fd30083f8f60599afc69d9eef7365
Emma Gerigscott <elgerig@gmail.com>
2023-05-09 11:56:57 -0700

    fix the problems that we created
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- CarDeals/Program.cs
+++ CarDeals/Program.cs
@@ -1,10 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace CarDeals
 {
   class Program
   {
-    static void Main()
+    static void Main(string[] args)
     {
-      
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
     }
   }
 }
