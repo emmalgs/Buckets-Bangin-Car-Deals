546eb6c62ddaf432fc1fb4a0fe73060f72178ac9
Emma Gerigscott <elgerig@gmail.com>
2023-05-09 10:05:07 -0700

    change class name to CarLot
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

---
+++ CarDeals/Models/CarLot.cs
@@ -0,0 +1,15 @@
+// using System;
+using System.Collections.Generic;
+
+namespace CarDeals.Models
+{
+  public abstract class CarLot
+  {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public float Price { get; set; }
+    public static List<CarLot> Options { get; set; } = new List<CarLot> {};
+  }
+}
