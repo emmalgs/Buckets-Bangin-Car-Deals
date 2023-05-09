c7f765d864babaa8b225b3bd697d8bf1d33b2cd9
Emma Gerigscott <elgerig@gmail.com>
2023-05-09 10:06:26 -0700

    add list test + passing test
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- CarDeals.Tests/ModelTests/CarTests.cs
+++ CarDeals.Tests/ModelTests/CarTests.cs
@@ -8,10 +8,19 @@ namespace CarDeals.Tests
   public class CarTests
   {
     [TestMethod]
-    public void Car_CreatesInstanceofCar_Car()
+    public void Trucks_CreatesInstanceofTrucks_Trucks()
     {
-      Cars bronco = new Cars();
-      Assert.AreEqual(typeof(Cars), bronco.GetType());
+      Trucks bronco = new Trucks("ford", "bronco", 1975, "blue", 72500);
+      Assert.AreEqual(typeof(Trucks), bronco.GetType());
+    }
+
+    [TestMethod]
+    public void Trucks_AddsTruckToOptions_List()
+    {
+      Trucks bronco = new Trucks("ford", "bronco", 1975, "blue", 72500);
+      Trucks.Options.Add(bronco);
+      string expected = "ford";
+      Assert.AreEqual(expected, Trucks.Options[0].Make);
     }
   }
 }
