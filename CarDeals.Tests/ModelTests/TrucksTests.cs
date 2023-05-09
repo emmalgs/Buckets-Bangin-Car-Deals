b65569d925af28f3aa831d251a32fe180283d6c8
Emma Gerigscott <elgerig@gmail.com>
2023-05-09 11:01:11 -0700

    change file name to TrucksTest + passing test
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- CarDeals.Tests/ModelTests/TrucksTests.cs
+++ CarDeals.Tests/ModelTests/TrucksTests.cs
@@ -3,10 +3,14 @@ using System;
 using CarDeals.Models;
 
 namespace CarDeals.Tests
-{
+{  
   [TestClass]
-  public class CarTests
+  public class CarTests : IDisposable
   {
+    public void Dispose()
+    {
+      Trucks.ClearAll();
+    }
     [TestMethod]
     public void Trucks_CreatesInstanceofTrucks_Trucks()
     {
@@ -19,10 +23,8 @@ namespace CarDeals.Tests
     {
       Trucks bronco = new Trucks("ford", "bronco", 1975, "blue", 72500);
       Trucks wagoneer = new Trucks("jeep", "wagoneer", 1964, "yellow", 64000);
-      Trucks.Options.Add(bronco);
-      Trucks.Options.Add(wagoneer);
       string expected = "jeep";
-      Assert.AreEqual(expected, Trucks.Options[1].Make);
+      Assert.AreEqual(expected, Trucks.All[1].Make);
     }
   }
 }
