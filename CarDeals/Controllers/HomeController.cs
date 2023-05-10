1a52dc37c31b4cc312556637c31641cd7dd432fd
Emma Gerigscott <elgerig@gmail.com>
2023-05-09 17:04:00 -0700

    WIP: add allcars page to show all cars added to lot
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- CarDeals/Controllers/HomeController.cs
+++ CarDeals/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-// using CarDeals.Models;
-// using System.Collections.Generic;
+using CarDeals.Models;
+using System.Collections.Generic;
+using System;
 
 namespace CarDeals.Controllers
 {
@@ -11,6 +12,31 @@ namespace CarDeals.Controllers
     public ActionResult Index()
     {
       return View();
+    }
+    [HttpPost("/carlot")]
+    public ActionResult Carlot(string[] car)
+    {
+      string type = car[0];
+      if(type == "trucks")
+      {
+        Trucks truck = new Trucks(car[1], car[2], int.Parse(car[3]), car[4], int.Parse(car[5]));
+        return View("Carlot", truck);
+      }
+      else if(type == "suvs")
+      {
+        SUVs suv = new SUVs(car[1], car[2], int.Parse(car[3]), car[4], int.Parse(car[5]));
+        return View("Carlot", suv);
+      }
+      else
+      {
+        Sedans sedan = new Sedans(car[1], car[2], int.Parse(car[3]), car[4], int.Parse(car[5]));
+        return View("Carlot", sedan);
+      }
+    }
+    [HttpGet("/allcars")]
+    public ActionResult AllCars()
+    {
+      return View("Allcars");
     }
   }
 }
